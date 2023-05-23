2a8571a28920fa6ef0bbaf7a3fe118f64fc1e567
Átila <112980460+Relieff@users.noreply.github.com>
2023-05-22 23:33:07 -0300

    Biblioteca de cores.py
    A simple python color library.

--- Biblioteca de cores.py
+++ Biblioteca de cores.py
@@ -1,16 +1,16 @@
-coresletras = {'limpa': '\033[m',
-               'azul': '\033[34m',
-               'amarelo': '\033[33m',
-               'vermelho': '\333[31m',
-               'pretoebranco': '\033[7;30m'}
-coresbg = {'branco': '\033[107m',
-            'vermelho': '\033[41m',
-            'verde' : '\033[42m,',
-            'amarelo' : '\033[43m',
-            'azul': '\033[44m',
-            'magenta': '\033[45m',
-            'ciano' : '\033[46m',
-            'limpa': '\033[m',
+coresletras = {'limpa':'\033[m',
+               'azul':'\033[34m',
+               'amarelo':'\033[33m',
+               'vermelho':'\333[31m',
+               'pretoebranco':'\033[7;30m'}
+coresbg = {'branco':'\033[107m',
+            'vermelho':'\033[41m',
+            'verde':'\033[42m,',
+            'amarelo':'\033[43m',
+            'azul':'\033[44m',
+            'magenta':'\033[45m',
+            'ciano':'\033[46m',
+            'limpa':'\033[m',
             'cinza':'\033[47m'}
 nome = str(input('Qual seu nome?'))
-print('Olá, seja bem vindo {}{}{}, prazer em te conhecer'.format(coresletras['amarelo'], coresbg['branco'], nome , coresletras['limpa'], coresbg['limpa']))
+print('Olá, seja bem vindo {}{}{}, prazer em te conhecer'.format(coresletras['amarelo'],coresbg['branco'],nome,coresletras['limpa'],coresbg['limpa']))
